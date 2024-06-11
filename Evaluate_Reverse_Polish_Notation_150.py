7753484cb5aa909791c82f50c7bea4c2a32b963b
MarkThompson5318 <mthompson@steamshipauthority.com>
2024-06-11 11:29:11 -0400

    finished polish notation

---
+++ Evaluate_Reverse_Polish_Notation_150.py
@@ -0,0 +1,52 @@
+from typing import List, Optional
+
+"""
+Below is the leetcode solution
+
+"""
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        func = {
+            "+": lambda x,y: x + y,
+            "-": lambda x,y: y - x,
+            "*": lambda x,y: x * y,
+            "/": lambda x,y: int(y / x)
+        }
+        while len(tokens) > 0:
+            tmp = tokens.pop(0)
+            if tmp.isnumeric() or (tmp[0]=='-' and tmp[1:].isdigit()):
+                stack.insert(0,int(tmp))
+            else:
+                a = stack.pop(0)
+                b = stack.pop(0)
+                result = func[tmp](a,b)
+                stack.insert(0,result)
+        return stack[0]
+
+"""
+Testing Below this
+
+"""
+    
+if __name__ == "__main__":
+    sol = Solution()
+    
+    allPass = True
+    errorMsg = "Value not correct, Value: {}"
+    
+    tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+    expected = 22
+
+    result1 = sol.evalRPN(tokens)
+    
+    try:
+        assert result1 == expected, errorMsg.format(result1)
+    except Exception as e: 
+        allPass = False
+        print("first test failed: {}".format(e))
+
+    if allPass:    
+        print("Passes all test cases")
+    
